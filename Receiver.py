a64ea0137ee6212d78fa1e96654785041febd21b
techprasanna <prasanna.bhope@gmail.com>
2022-03-28 11:32:08 -0400

    Updated Code. Handles multi-threading

--- Receiver.py
+++ Receiver.py
@@ -1,3 +1,5 @@
+import struct
+
 import cv2
 from threading import Thread
 import numpy as np
@@ -7,36 +9,53 @@ import pickle
 from datetime import datetime
 from Packet import Packet_Structure
 import socket, threading
-
+from main import Main
 class Receiver(Thread):
 
     diff = 0
     counter = 0
+    flag = True
+    diff = []
+    m = Main()
 
-    def __init__(self, clientAddress, clientsocket):
+    def __init__(self, clientAddress, clientsocket, cloud_socket):
         threading.Thread.__init__(self)
-        self.csocket = clientsocket
+        self.conn = clientsocket
+        self.cloud_socket = cloud_socket
+        self.clientAddress = clientAddress
         print("New connection added: ", clientAddress)
 
-    def run(self, clientAddress=None):
-        # print ("Connection from : ", clientAddress)
-        # self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        msg = ''
-        # Instead of this while True method, now we have to perform face detection
+    def run(self):
+        data = b""
+        payload_size = struct.calcsize("L")
         while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg == 'bye':
-                break
-            print("from client", msg)
-            self.csocket.send(bytes(msg, 'UTF-8'))
-        print("Client at ", clientAddress, " disconnected...")
+            while len(data) < payload_size:
+                data += self.conn.recv(4096)
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += self.conn.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            ###
 
-    def face_detector_test(self, image):
+            frame = pickle.loads(frame_data)
+            # print ("Hello")
+            self.face_detector_test(frame)
+
+    def face_detector_test(self,image):
         # imagePath = "/Users/prasannasmac/Documents/Capstone/test_image3.jpeg"
 
         # image = cv2.imread(imagePath)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # global flag
+        # if flag:
+        #     global diff
+        #     diff = image
+        #     flag = False
+        # print(mse(diff,image))
+        # diff = image
 
         faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         faces = faceCascade.detectMultiScale(
@@ -51,44 +70,46 @@ class Receiver(Thread):
         # print(faces)
         face_list = []
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi_color = image[y:y + h, x:x + w]
             face_list.append(roi_color)
-            # print(len(face_list))
-            print("[INFO] Object found. Saving locally.")
-            cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
-            if roi_color is not None:
-                self.packetize_images(roi_color)
+            if self.flag:
+                self.diff = image
+                self.flag = False
+            if (self.mse(self.diff, image)) > 3000:
+                self.diff = image
+                # print(type(roi_color))
+                # print(len(face_list))
+                print("[INFO] Object found. Saving locally.")
+                # cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
+                if roi_color is not None:
+                    self.packetize_images(roi_color)
 
         # status = cv2.imwrite('faces_detected.jpg', image)
         # print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
         # return face_list, faces
-    # def catch_faceList(self, alist):
-    #     print(alist)
-    #     print(len(alist))
 
-    # def start_face_detection(self):
-    #     # faces = self.face_detector_test()
-    #
-    #     # print(len(faces))
-    #     # cv2.imwrite(self.id+'faces.jpg', face_list[0])
-    #
-    #     if(len(faces) > 0):
-    #         self.packetize_images(faces)
+    def packetize_images(self,faces):
 
-    def packetize_images(self, faces):
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = 12345
-
-        s.connect(('127.0.0.1', port))
         now = datetime.now()
 
         current_time = now.strftime("%H:%M:%S")
 
-        packet = Packet_Structure('192.168.07', len(faces), faces, '0011',str(current_time), 0)
+        packet = Packet_Structure(self.clientAddress, len(faces), faces, '0011', str(current_time), 0)
         data_string = pickle.dumps(packet)
-        s.send(data_string)
+        # s.send(data_string)
+        self.m.add_data(self.cloud_socket, data_string)
+
+    def mse(self, imageA, imageB):
+        # the 'Mean Squared Error' between the two images is the
+        # sum of the squared difference between the two images;
+        # NOTE: the two images must have the same dimension
+        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+        err /= float(imageA.shape[0] * imageA.shape[1])
+
+        # return the MSE, the lower the error, the more "similar"
+        # the two images are
+        return err
 
     # def run(self):
     #     # face_list = self.face_detector()
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
+import struct
 import time
 from Packet import Packet_Structure
 from threading import Thread
-from Receiver import Receiver
+import Receiver
 import socket
 import socket, threading
 
 class Main():
     queue = []
+    threadLock = threading.Lock()
 
-    def send_data(self):
-        if(len(self.queue)) != 0:
-            return self.queue.pop(0)
-        else:
-            return None
+    def send_data(self, cloud_socket, packet):
+        self.threadLock.acquire()
+        cloud_socket.sendall(struct.pack("L", len(packet)) + packet)
+        self.queue.pop(0)
+        self.threadLock.release()
 
-    def add_data(self, packet):
+    def add_data(self, cloud_socket, packet):
         self.queue.append(packet)
+        while len(self.queue) != 0:
+            self.send_data(cloud_socket, self.queue[0])
+
+
 
     if __name__ == '__main__':
+        #Sending data to cloud. Used cloud socket to transfer images to cloud server. Port = 12345
+        cloud_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = 12345
+        cloud_socket.connect(('127.0.0.1', port))
+
+        # Receiving Data from the edge device
         LOCALHOST = "127.0.0.1"
-        PORT = 8080
+        PORT = 8089
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind((LOCALHOST, PORT))
@@ -28,7 +40,7 @@ class Main():
         while True:
             server.listen(1)
             clientsock, clientAddress = server.accept()
-            newthread = Receiver(clientAddress, clientsock)
+            newthread = Receiver.Receiver(clientAddress, clientsock, cloud_socket)
             newthread.start()
         # receiver = Receiver('127.0.0.1',"Prasanna")
         # receiver.start()
